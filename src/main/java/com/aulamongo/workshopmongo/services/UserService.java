f850ebd727754b8a497805a7965f0351b0bdb000
Matheus Pereira <matheus_osgarotoes@hotmail.com>
2026-09-01 18:41:10 -0300

    Inserção de usuário com POST

--- src/main/java/com/aulamongo/workshopmongo/services/UserService.java
+++ src/main/java/com/aulamongo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aulamongo.workshopmongo.domain.User;
+import com.aulamongo.workshopmongo.dto.UserDTO;
 import com.aulamongo.workshopmongo.repository.UserRepository;
 import com.aulamongo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +27,13 @@ public class UserService {
     		throw new ObjectNotFoundException("Objeto nao encontrado");
     	}
     	return user.get();
+    }
+    
+    public User insert(User obj) {
+    	return repo.insert(obj);    
+    }
+    
+    public User fromDTO(UserDTO objDto) {
+    	return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
