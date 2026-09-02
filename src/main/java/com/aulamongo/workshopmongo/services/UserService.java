7d852e0d9a005ba2107e7aa40cb4322e3da3d10c
Matheus Pereira <matheus_osgarotoes@hotmail.com>
2026-09-02 19:16:49 -0300

    Atualização de usuário com PUT

--- src/main/java/com/aulamongo/workshopmongo/services/UserService.java
+++ src/main/java/com/aulamongo/workshopmongo/services/UserService.java
@@ -39,7 +39,18 @@ public class UserService {
     	return obj;
     }
     
-    public User fromDTO(UserDTO objDto) {
+    public User update(User obj) {
+    	User newObj = repo.findById(obj.getId()).orElseThrow();
+    	updateData(newObj, obj);
+    	return repo.save(newObj);
+    }
+    
+    private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
     	return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
